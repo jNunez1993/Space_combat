02021c69c7290ce6f9a72c79c4190c6bad779022
Juan Nunez <njuan1993@yahoo.com>
2016-11-25 15:31:38 -0500

    imgs for ships

--- src/state/play/play_view.py
+++ src/state/play/play_view.py
@@ -2,8 +2,12 @@ import pygame
 
 class PlayView:
 	def __init__(self):
-		pass
+		self.window = pygame.display.get_surface()
+		self.starship = pygame.image.load("../img/ship1_north.png")
+		
 
 	def render(self):
-		pass
-		#print "Rendering play view"
+		self.window.fill((0,0,0))
+		self.window.blit(self.starship, self.starship.get_rect())
+		pygame.display.flip()
+		
