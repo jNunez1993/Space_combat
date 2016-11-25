95d798e67d551a709cc5df99d3b4ffab8c170c2b
Juan Nunez <njuan1993@yahoo.com>
2016-11-25 00:56:30 -0500

    Accepting client key presses

--- networking/player_thread.py
+++ networking/player_thread.py
@@ -1,4 +1,5 @@
 from threading import Thread
+import json
 
 #Responsible for receiving player information and updating its player
 #All of these players will be fed to the map object
@@ -13,7 +14,15 @@ class PlayerThread(Thread):
 		return self.player
 
 	#overridden method
+	#will listen for key pressed and update the player as needed
+	#Would be good if it can inform the map broadcaster that a change has occured
 	def run(self):
-		pass
-		#while True:
-			#print "Listening for client_id : " + str(self.player.getId())
+		print "Checking for key pressed"
+		while True:
+			print "waiting for key"
+			data = self.socket.recv(1024)
+			data = json.loads(data)
+			keyPressed = data["keyPressed"]
+			print "key received:" + str(keyPressed)
+			msg = {"msg" : "kpACK"}
+			self.socket.sendall(json.dumps(msg))
