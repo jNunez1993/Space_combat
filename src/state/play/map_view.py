603330549458fc9686651aeda8a5b7e0db34284d
Juan Nunez <njuan1993@yahoo.com>
2016-11-26 00:49:03 -0500

    Different colored projectiles and smaller radius

--- src/state/play/map_view.py
+++ src/state/play/map_view.py
@@ -16,6 +16,7 @@ class MapView:
 
 
 	def render(self):
+		img = None
 		for player in self.players:
 			img = self.getPlayerRepresentation(player)
 			x = player["location"]["x"]
@@ -27,8 +28,12 @@ class MapView:
 		for projectile in self.projectiles:
 			x = projectile["location"]["x"]
 			y = projectile["location"]["y"]
+			dis = self.bulletDisplacement(projectile["direction"],img.get_rect())
 
-			pygame.draw.circle(self.window,(255,255,255),(x,y),10)
+			if projectile["id"] == 1:
+				pygame.draw.circle(self.window,(255,0,0),(x+dis[0],y+dis[1]),3)
+			if projectile["id"] == 2:
+				pygame.draw.circle(self.window,(0,255,0),(x+dis[0],y+dis[1]),3)
 
 		pygame.display.flip()
 
@@ -49,3 +54,35 @@ class MapView:
 		self.gameMap = gameMap
 		self.players = self.gameMap['players']
 		self.projectiles = self.gameMap['projectiles']
+
+
+	def bulletDisplacement(self, direction,rect):
+		width = rect.width
+		height = rect.height
+		disX = 0
+		disY = 0
+		if direction == "north":
+			disX = width/2
+		elif direction == "east":
+			disX = width
+			disY = height/2
+		elif direction == "south":
+			disX = width/2
+			disY = height
+		elif direction == "west":
+			disY = height/2
+		elif direction == "northeast":
+			disX = width
+		elif direction == "northwest":
+			pass
+		elif direction == "southeast":
+			disX = width
+			disY = height
+		elif direction == "southwest":
+			disY = height
+
+		return (disX,disY)
+
+
+
+
